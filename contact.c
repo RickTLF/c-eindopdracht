204b9f10e53452bbf6b0651b4342ec073e7317c4
Rick <rit.kock@student.avans.nl>
2015-11-29 11:29:31 +0100

    Update contact.c

--- contact.c
+++ contact.c
@@ -1,22 +1,22 @@
 #include "contact.h"
 
-//Assigns both name and number to contact
+/*!Assigns both name and number to contact*/
 void assignContactValues( Contact *contact, char *name, char *number ) {
     contact -> name = name;
     contact -> phoneNumber = number;
 }
 
-//Assigns number to a contact
+/*!Assigns number to a contact*/
 void assignContactNumber( Contact *contact, char *number ) {
     contact -> phoneNumber = number;
 }
 
-//Assigns name to a contact
+/*!Assigns name to a contact*/
 void assignContactName( Contact *contact, char *name ) {
     contact -> name = name;
 }
 
-//Prints out contact info
+/*!Prints out contact info*/
 void printContactInfo( Contact *contact ) {
     printf("Name of contact %s\n", contact -> name);
     printf("Phonenumber of contact %s\n", contact -> phoneNumber);
